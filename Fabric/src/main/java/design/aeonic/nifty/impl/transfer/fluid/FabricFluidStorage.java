fad5edc25bd40badd0eb0d27384d55d78ab92029
aeonic <io@aeonic.design>
2022-11-10 20:48:15 -0600

    Add FabricAspects implementation, fabric fluid & item handlers

---
+++ Fabric/src/main/java/design/aeonic/nifty/impl/transfer/fluid/FabricFluidStorage.java
@@ -0,0 +1,118 @@
+package design.aeonic.nifty.impl.transfer.fluid;
+
+import com.google.common.collect.Iterators;
+import design.aeonic.nifty.api.transfer.fluid.FluidStack;
+import design.aeonic.nifty.api.transfer.fluid.FluidStorage;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+import net.fabricmc.fabric.api.transfer.v1.storage.StorageView;
+import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Predicate;
+
+@SuppressWarnings("UnstableApiUsage")
+public class FabricFluidStorage implements FluidStorage {
+    private final Storage<FluidVariant> fluidVariantStorage;
+
+    public FabricFluidStorage(Storage<FluidVariant> fluidVariantStorage) {
+        this.fluidVariantStorage = fluidVariantStorage;
+    }
+
+    @Override
+    public int getSlots() {
+        return Iterators.size(fluidVariantStorage.iterator());
+    }
+
+    @Override
+    public FluidStack getStackInSlot(int slot) {
+        try (Transaction transaction = Transaction.getCurrentUnsafe().openNested()) {
+            for (StorageView<FluidVariant> view : fluidVariantStorage) {
+                if (slot == 0) {
+                    return variantToStack(view.getResource(), view.getAmount());
+                }
+                slot--;
+            }
+        }
+        return FluidStack.EMPTY_STACK;
+    }
+
+    @Override
+    public long getSlotCapacity(int slot) {
+        try (Transaction transaction = Transaction.getCurrentUnsafe().openNested()) {
+            for (StorageView<FluidVariant> view : fluidVariantStorage) {
+                if (slot == 0) {
+                    return view.getCapacity();
+                }
+                slot--;
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean isEverValid(int slot, FluidStack stack) {
+        // TODO: Probably a better way to do this, but whatever. We just skip this check; the performance
+        // hit should be less than what would result from iterating through storage views.
+        return true;
+    }
+
+    @Override
+    public FluidStack insert(int slot, @NotNull FluidStack stack, boolean simulate) {
+        return insert(stack, simulate);
+    }
+
+    @Override
+    public FluidStack insert(@NotNull FluidStack stack, boolean simulate) {
+        try (Transaction transaction = Transaction.getCurrentUnsafe().openNested()) {
+            long amountInserted = simulate ? fluidVariantStorage.simulateInsert(stackToVariant(stack), stack.getAmount(), transaction) :
+                    fluidVariantStorage.insert(stackToVariant(stack), stack.getAmount(), transaction);
+            if (amountInserted == 0) return stack;
+
+            FluidStack result = stack.copy();
+            result.shrink((int) amountInserted);
+            return result;
+        }
+    }
+
+    @Override
+    public FluidStack extract(Predicate<FluidStack> filter, long amount, boolean simulate) {
+        try (Transaction transaction = Transaction.getCurrentUnsafe().openNested()) {
+            for (StorageView<FluidVariant> view : fluidVariantStorage) {
+                FluidVariant resource = view.getResource();
+                if (filter.test(variantToStack(resource, 1))) {
+                    long amountExtracted = simulate ? fluidVariantStorage.simulateExtract(resource, amount, transaction) : fluidVariantStorage.extract(resource, amount, transaction);
+                    if (amountExtracted == 0) return FluidStack.EMPTY_STACK;
+
+                    return variantToStack(resource, amountExtracted);
+                }
+            }
+        }
+        return FluidStack.EMPTY_STACK;
+    }
+
+    @Override
+    public FluidStack extract(int slot, long amount, boolean simulate) {
+        try (Transaction transaction = Transaction.getCurrentUnsafe().openNested()) {
+            for (StorageView<FluidVariant> view : fluidVariantStorage) {
+                FluidVariant resource = view.getResource();
+                if (slot == 0) {
+                    long amountExtracted = simulate ? fluidVariantStorage.simulateExtract(resource, amount, transaction) : fluidVariantStorage.extract(resource, amount, transaction);
+                    if (amountExtracted == 0) return FluidStack.EMPTY_STACK;
+
+                    return variantToStack(resource, amountExtracted);
+                }
+                slot--;
+            }
+        }
+        return FluidStack.EMPTY_STACK;
+    }
+
+    public static FluidStack variantToStack(FluidVariant variant, long amount) {
+        return FluidStack.of(variant.getFluid(), amount, variant.copyNbt());
+    }
+
+    public static FluidVariant stackToVariant(FluidStack stack) {
+        return FluidVariant.of(stack.getFluid(), stack.getTag());
+    }
+}
---
+++ Fabric/src/main/java/design/aeonic/nifty/impl/transfer/item/FabricItemStorage.java
@@ -0,0 +1,110 @@
+package design.aeonic.nifty.impl.transfer.item;
+
+import com.google.common.collect.Iterators;
+import design.aeonic.nifty.api.transfer.item.ItemStorage;
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+import net.fabricmc.fabric.api.transfer.v1.storage.StorageView;
+import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Predicate;
+
+@SuppressWarnings("UnstableApiUsage")
+public class FabricItemStorage implements ItemStorage {
+    private final Storage<ItemVariant> itemVariantStorage;
+
+    public FabricItemStorage(Storage<ItemVariant> itemVariantStorage) {
+        this.itemVariantStorage = itemVariantStorage;
+    }
+
+    @Override
+    public int getSlots() {
+        return Iterators.size(itemVariantStorage.iterator());
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int slot) {
+        try (Transaction transaction = Transaction.getCurrentUnsafe().openNested()) {
+            for (StorageView<ItemVariant> view : itemVariantStorage) {
+                if (slot == 0) {
+                    return view.getResource().toStack((int) Math.min(view.getAmount(), Integer.MAX_VALUE));
+                }
+                slot--;
+            }
+        }
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public long getSlotCapacity(int slot) {
+        try (Transaction transaction = Transaction.getCurrentUnsafe().openNested()) {
+            for (StorageView<ItemVariant> view : itemVariantStorage) {
+                if (slot == 0) {
+                    return view.getCapacity();
+                }
+                slot--;
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean isEverValid(int slot, ItemStack stack) {
+        // TODO: Probably a better way to do this, but whatever. We just skip this check; the performance
+        // hit should be less than what would result from iterating through storage views.
+        return true;
+    }
+
+    @Override
+    public ItemStack insert(int slot, @NotNull ItemStack stack, boolean simulate) {
+        return insert(stack, simulate);
+    }
+
+    @Override
+    public ItemStack insert(@NotNull ItemStack stack, boolean simulate) {
+        try (Transaction transaction = Transaction.getCurrentUnsafe().openNested()) {
+            long amountInserted = simulate ? itemVariantStorage.simulateInsert(ItemVariant.of(stack), stack.getCount(), transaction) :
+                    itemVariantStorage.insert(ItemVariant.of(stack), stack.getCount(), transaction);
+            if (amountInserted == 0) return stack;
+
+            ItemStack result = stack.copy();
+            result.shrink((int) amountInserted);
+            return result;
+        }
+    }
+
+    @Override
+    public ItemStack extract(Predicate<ItemStack> filter, long amount, boolean simulate) {
+        try (Transaction transaction = Transaction.getCurrentUnsafe().openNested()) {
+            for (StorageView<ItemVariant> view : itemVariantStorage) {
+                ItemVariant resource = view.getResource();
+                if (filter.test(resource.toStack())) {
+                    long amountExtracted = simulate ? itemVariantStorage.simulateExtract(resource, amount, transaction) : itemVariantStorage.extract(resource, amount, transaction);
+                    if (amountExtracted == 0) return ItemStack.EMPTY;
+
+                    return resource.toStack((int) amountExtracted);
+                }
+            }
+        }
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public ItemStack extract(int slot, long amount, boolean simulate) {
+        try (Transaction transaction = Transaction.getCurrentUnsafe().openNested()) {
+            for (StorageView<ItemVariant> view : itemVariantStorage) {
+                ItemVariant resource = view.getResource();
+                if (slot == 0) {
+                    long amountExtracted = simulate ? itemVariantStorage.simulateExtract(resource, amount, transaction) : itemVariantStorage.extract(resource, amount, transaction);
+                    if (amountExtracted == 0) return ItemStack.EMPTY;
+
+                    return resource.toStack((int) amountExtracted);
+                }
+                slot--;
+            }
+        }
+        return ItemStack.EMPTY;
+    }
+}
