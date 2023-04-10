162cb12b8dc0aeab92664049f1441903d4a87873
aeonic <io@aeonic.design>
2023-04-10 17:13:28 -0500

    commit some unstaged files so i can delete my local repo

---
+++ Common/src/main/java/design/aeonic/nifty/impl/content/NiftyBlocks.java
@@ -0,0 +1,12 @@
+package design.aeonic.nifty.impl.content;
+
+import design.aeonic.nifty.api.util.Registrar;
+import net.minecraft.world.level.block.Block;
+
+public final class NiftyBlocks {
+//    public static final Block
+
+    public static void register(Registrar<Block> registrar) {
+
+    }
+}
